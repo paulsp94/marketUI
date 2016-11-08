07ad1f6e110fb2540031261a9fb8e7dd9f9e1697
kohze <mlw.kohze@student.ru.nl>
2016-11-08 22:20:41 +0100

    added login and logout

---
+++ client/components/account/logout.jsx
@@ -0,0 +1,37 @@
+import * as firebase from 'firebase';
+import React, { Component } from 'react';
+var AppActions = require('../../Action/AppActions');
+var AppStore = require('../../Stores/AppStore');
+import {Link} from "react-router";
+import {withRouter} from 'react-router';
+import { hashHistory } from 'react-router';
+import Header from '../Header/Header.jsx';
+import Subheader from '../Subheader/Subheader.jsx';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import Slider from 'material-ui/Slider';
+import ReactMarkdown from 'react-markdown';
+
+var logout = React.createClass({
+    contextTypes: {
+        router: React.PropTypes.object.isRequired
+    },
+    getInitialState: function(){
+        return {
+            error: false
+        }
+    },
+    componentDidMount: function () {
+        firebase.auth().signOut();
+        this.setState({loggedIn: false});
+        // this.context.router.replace('/');
+    },
+    render: function () {
+        return <p>You are now logged out</p>;
+    }
+});
+
+module.exports = logout;
--- client/main.jsx
+++ client/main.jsx
@@ -1,5 +1,4 @@
 'use strict';
-
 import 'styles/main.css';
 import React from 'react';
 import { render } from 'react-dom';
@@ -11,8 +10,9 @@ import ItemPreview from 'components/ItemPreview/ItemPreview';
 import Downloads from 'components/Downloads/Downloads';
 import General from 'components/Product/General.jsx';
 import ProductSearch from 'components/ProductSearch/ProductSearch.jsx';
-import registration from 'components/registration/registration.jsx';
-
+import registration from 'components/account/registration.jsx';
+import login from 'components/account/login.jsx';
+import logout from 'components/account/logout.jsx';
 
   render(
       <Router history={hashHistory}>
@@ -21,6 +21,8 @@ import registration from 'components/registration/registration.jsx';
           <Route path="General" component={General}/>
           <Route path="ProductSearch" component={ProductSearch}/>
           <Route path="registration" component={registration}/>
+          <Route path="login" component={login}/>
+          <Route path="logout" component={logout}/>
           <Route path="/" component={Downloads}/>
       </Router>,
       document.getElementById('js-main'));
