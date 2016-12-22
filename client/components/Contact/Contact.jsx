426fac68d5f36fd9d3d24fae94cd4df7deb623ff
Daniyel <superdev555@hotmail.com>
2016-12-22 15:50:47 +0100

    Contact Page Boxed & MoreInfo Page Added

--- client/components/Contact/Contact.jsx
+++ client/components/Contact/Contact.jsx
@@ -1,44 +1,43 @@
 import React, { Component } from 'react';
-import {Link} from "react-router";
-import {withRouter} from 'react-router';
-import { hashHistory } from 'react-router';
-import Subheader from '../Subheader/Subheader.jsx';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import RaisedButton from 'material-ui/RaisedButton';
-import Flexbox from 'flexbox-react';
 import TextField from 'material-ui/TextField';
+import {orange500, blue500} from 'material-ui/styles/colors';
 import Divider from 'material-ui/Divider/Divider';
 
 <meta name="viewport" content="width=device-width" />
 
 var Contact = React.createClass({ 
     render(){
-        var style = { backgroundColor: 'white',
-                      margin: 'auto',
-                      marginTop: '40px', 
-                      height: '80%', 
-                      width: '60%'
-                  };
         return (
-                <div style={style}>
-                    <div id={'contact_div'}>
-                        <span>Please Input Your Message :</span>    
-                        <TextField hintText={'Message'} /*floatingLabelText={'Message'}*/ className={'contact_text'}/> 
+               
+                    <div className={'container-contact'}>
+                      <div style={{}}>
+                        <span> Your Email :</span>    
+                        <TextField hintText={'Email'} /*floatingLabelText={'Message'}*/ className={'contact_text'}/> 
                          <br />    
-                        <span style={{paddingLeft:'88px'}}>Your Address :</span>    
-                        <TextField hintText={'Address'} /*floatingLabelText={'Message'}*/ className={'contact_text'}/>
+                        <span >Your Name :</span>    
+                        <TextField hintText={'Name'} /*floatingLabelText={'Message'}*/ className={'contact_text'}/>
                         <br />    
-                        <span style={{paddingLeft:'38px'}}>Your Company Name :</span>    
-                        <TextField hintText={'Company Name'} /*floatingLabelText={'Message'}*/ className={'contact_text'}/>
+                        <span>&nbsp;&nbsp;&nbsp;Message :</span>    
+                        <TextField hintText={'Message'} /*floatingLabelText={'Message'}*/ className={'contact_text'}/>
                         <br />
                         <br />
+                        <div>    
+                         <RaisedButton primary={true} label="Contact" href="" />
+                        </div>
+                        <br />
+                      </div>
+                        
                         <Divider /> 
                         <br />
-                        <div style={{padding:'0 45%'}}>    
-                         <RaisedButton primary={true} label="Contact" href="" />
-                        </div>  
-                    </div> 
-                </div>
+                        <div style={{lineHeight:'30px',border:'1px solid lightgrey'}}>
+                          <span>Company Name : Rscript.Market</span><br />
+                          <span>Company Address :     </span><br />
+                          <span>Company Phone Number : </span><br />
+                          <span>Company Email Address :</span><br />
+                        </div>
+                    </div>
+
         )
     }
 
