54c4cc7e4ae86945868c2fffe109f97fbc7c33be
Kohze <mlw.kohze@student.ru.nl>
2017-01-01 18:15:05 +0100

    additional fixes

--- client/components/Downloads/DataContainer.jsx
+++ client/components/Downloads/DataContainer.jsx
@@ -33,15 +33,13 @@ class  DataContainer extends React.Component{
 
     render(){
 
-        var Style = {
-        };
-
         return (
-            <Card style={{ marginRight: "2%", marginLeft: "2%", marginTop: 9}} onClick={this.updateItem.bind(this)}
+            <Card   style={{ marginRight: "2%", marginLeft: "2%", marginTop: 9}} 
+                    onClick={this.updateItem.bind(this)}
             >
             <div className="Product" >
                 <CardText>
-                    <img className="productimage" src={this.props.item.Subimage} style = {Style}/>
+                    <img className="productimage" src={this.props.item.Subimage}/>
                 <div className="text-part">
                 <h4> <strong> {this.props.item.Title} </strong> </h4>
                     <RaisedButton onClick={this.ProductContentDownload.bind(this)} label="Download" style={{ margin: 12}} />
