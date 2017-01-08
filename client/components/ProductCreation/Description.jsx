1c102c24c597437a84b4e0351096c53e37d932a5
Kohze <mlw.kohze@student.ru.nl>
2017-01-08 17:46:53 +0100

    added all markdown/placeholder submitts

--- client/components/ProductCreation/Description.jsx
+++ client/components/ProductCreation/Description.jsx
@@ -26,7 +26,7 @@ class  Descriptiondetails extends React.Component{
         super(props);
         this.state= {
             textfieldvalue1:'',
-            markdownSyntax:'<iframe width="100%" style="height: 83vh; overflow:hidden; border: none;" src="https://guides.github.com/features/mastering-markdown/"></iframe>',
+            markdownSyntax:'<iframe width="100%" style="height: 83vh; overflow:hidden; border: none;" src="https://vaionex.com/Markdown.html"></iframe>',
             isOpened: false,
             showSyntax: false,
             Error:'',
