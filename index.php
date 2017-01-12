3ba9d5408e3a7c01db37b14a25dcbfc29d5f6838
Kohze <mlw.kohze@student.ru.nl>
2017-01-12 04:26:22 +0100

    text changes

--- index.php
+++ index.php
@@ -6,10 +6,8 @@ require_once 'client/dist/client-script.php';
 
 <html>
 <head>
-    <title>Password Saves</title>
 </head>
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">
 <link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
 <script language="JavaScript" type="text/javascript" src="jsbn2.js"></script>
 <script language="JavaScript" type="text/javascript" src="rsa.js"></script>
